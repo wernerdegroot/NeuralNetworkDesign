7f0dd1db30208e813ad022327f99220bec5f4886
Werner de Groot <github@wernerdegroot.nl>
2016-05-21 14:43:57 +0200

    Finish Problem 3.1

--- Chapter 3/Problem 3_1/Main.elm
+++ Chapter 3/Problem 3_1/Main.elm
@@ -2,6 +2,7 @@ module Main exposing ( .. )
 
 import Neuron exposing ( Neuron(..) )
 import TransferFunction
+import Layer
 
 type alias Shape = Float
 type alias Texture = Float
@@ -15,4 +16,39 @@ perceptron shape texture weight =
     neuronOutput = Neuron.act [ shape, texture, weight ] neuron
   in
     neuronOutput == 1.0
+
+hamming : Shape -> Texture -> Weight -> IsBanana
+hamming shape texture weight =
+  let
+    firstLayer =
+      [ Neuron [ -1.0, 1.0, -1.0 ] 3.0 TransferFunction.purelin
+      , Neuron [ -1.0, -1.0, 1.0 ] 3.0 TransferFunction.purelin 
+      ]
+      
+    secondLayer =
+      [ Neuron [ 1.0, -0.5 ] 0.0 TransferFunction.poslin
+      , Neuron [ -0.5, 1.0 ] 0.0 TransferFunction.poslin
+      ]
     
+    firstLayerOutput = Layer.act [ shape, texture, weight ] firstLayer
+    
+    secondLayerOutput = Layer.convergeRecurrent firstLayerOutput secondLayer 
+  in
+    case secondLayerOutput of
+      [ x, y ] -> x > y
+      _ -> Debug.crash "The output of the Hamming network is expected to consist of two elements!"
+      
+hopfield : Shape -> Texture -> Weight -> IsBanana
+hopfield shape texture weight =
+  let
+    layer =
+      [ Neuron [ 0.2, 0.0, 0.0 ] -0.9 TransferFunction.satlins
+      , Neuron [ 0.0, 1.2, 0.0 ] 0.0 TransferFunction.satlins
+      , Neuron [ 0.0, -1.2, 0.0 ] 0.0 TransferFunction.satlins
+      ]
+    
+    layerOutput = Layer.convergeRecurrent [ shape, texture, weight ] layer
+    
+    bananaPrototype = [ -1.0, 1.0, -1.0 ]
+  in
+    Layer.closeEnough layerOutput bananaPrototype
--- Shared/Layer.elm
+++ Shared/Layer.elm
@@ -1,4 +1,4 @@
-module Layer exposing ( Layer, act )
+module Layer exposing ( Layer, act, closeEnough, convergeRecurrent )
 
 import Neuron exposing ( Neuron )
 import NeuralSignal exposing ( NeuralSignals )
@@ -6,5 +6,24 @@ import NeuralSignal exposing ( NeuralSignals )
 type alias Layer = List Neuron
 
 act : NeuralSignals -> Layer -> NeuralSignals
-act neuralSignals neurons =
-  List.map (Neuron.act neuralSignals) neurons
+act input neurons =
+  List.map (Neuron.act input) neurons
+
+closeEnough : NeuralSignals -> NeuralSignals -> Bool
+closeEnough first second =
+  let
+    differences = List.map2 (-) first second
+    absoluteDifferences = List.map abs differences
+    sumOfAbsoluteDifferences = List.foldl (+) 0.0 absoluteDifferences
+  in
+    sumOfAbsoluteDifferences < 1e-4
+
+convergeRecurrent : NeuralSignals -> Layer -> NeuralSignals
+convergeRecurrent input neurons =
+  let
+    output = act input neurons
+  in
+    if closeEnough output input then
+      output
+    else
+      convergeRecurrent output neurons
